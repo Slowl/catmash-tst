757f256f82f02291afdeb94d70c75ec238cefae6
Slowl <hedi0475@gmail.com>
2019-10-29 18:36:39 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+  width: 100vw;
+  background-color: #131313;
+  color: white;
+`
+
+class App extends React.Component {
+
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    fetch('https://cors-anywhere.herokuapp.com/https://latelier.co/data/cats.json')
+    .then(res => res.json())
+    .then(dataCats => this.setState({data : dataCats.images}))
+  }
+
+  render() {
+
+    const {data} = this.state
+
+    return (
+      <AppContainer>
+
+      </AppContainer>
+    )
+  }
 }
 
-export default App;
+export default App
