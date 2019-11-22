dcd60f2aa1c1bcca2d8e5c225d14e2fe61f3a931
Slowl <hedi0475@gmail.com>
2019-11-22 15:52:09 +0100

    netlify integration first try

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
   var firebaseConfig = {
-    apiKey: "AIzaSyCxXpYnb06isKSPTwaB8fv_j9cq1QmOavU",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "ca7mash.firebaseapp.com",
     databaseURL: "https://ca7mash.firebaseio.com",
     projectId: "ca7mash",
     storageBucket: "ca7mash.appspot.com",
     messagingSenderId: "831923527880",
-    appId: "1:831923527880:web:34cc1176a431794091c5fd"
+    appId: process.env.FIREBASE_APP_ID
   };
   firebase.initializeApp(firebaseConfig);
 
